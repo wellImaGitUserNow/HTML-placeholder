4b7e61adaca23542ac28699a30eec8e8ad7efea6
Seba Revi <aquawaser21@gmail.com>
2024-04-20 18:32:06 +0200

    formatting fixed; useles comments removed

--- src/extension.ts
+++ src/extension.ts
@@ -25,188 +25,144 @@ export function activate(context: vscode.ExtensionContext) {
 			{
 				editBuilder.insert(new vscode.Position(editor.selection.active.line, editor.selection.active.character), "\n");
 			});
-			//console.log("current line: " + editor?.selection.active.line + "\nline a character after: " + doc.positionAt(event.contentChanges[0].range.start.character + 1) + "\n");
 	});
 
 	context.subscriptions.push(disposable);
 
-	/*
+	const provideInlineCompletionItems = (document: vscode.TextDocument, position: vscode.Position, context: vscode.InlineCompletionContext) =>
+	{
+		let prefix = document.lineAt(position).text.substr(0, position.character);
 
-	let loremipsum = vscode.workspace.onDidChangeTextDocument(event =>
+		if(prefix.endsWith('/') && !prefix.endsWith("</"))
 		{
-			const editor = vscode.window.activeTextEditor;
+			const slashCompletionItemText = new vscode.InlineCompletionItem("lorem");
+			const slashCompletionItemImage = new vscode.InlineCompletionItem("image");
 
-			if(!editor)
-			{
-				return;
-			}
+			return [slashCompletionItemText, slashCompletionItemImage];
+		}
 
-			const doc = event.document;
-			const text = doc.getText();
+		if(prefix.endsWith("/lorem p ") || prefix.endsWith("/lorem l ") || prefix.endsWith("/lorem w "))
+		{
+			const noPlaceholder = new vscode.InlineCompletionItem("" + Math.round(Math.random() * 21));
 
+			return [noPlaceholder];
+		}
+
+		return [];
+	}
+
+	context.subscriptions.push(vscode.languages.registerInlineCompletionItemProvider(
+		["html", 'php'], 
+		{provideInlineCompletionItems}
+	));
+
+	const provideCompletionItems = (document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) =>
+	{
+		let prefix = document.lineAt(position).text.substr(0, position.character);		
+
+		if(prefix.endsWith("/lorem "))
+		{
+			const completionItems = 
+			[
+				new vscode.CompletionItem("p", vscode.CompletionItemKind.Text),
+				new vscode.CompletionItem("l", vscode.CompletionItemKind.Text),
+				new vscode.CompletionItem("w", vscode.CompletionItemKind.Text)
+			];
+
+			return completionItems;
+		}
+
+		return [];
+	}
+
+	context.subscriptions.push(vscode.languages.registerCompletionItemProvider(
+		["html", 'php'], 
+		{provideCompletionItems},
+		' '
+	));
+
+	var match;
+	var decoration = vscode.window.createTextEditorDecorationType(
+		{
+
+		}
+	);
+
+	vscode.workspace.onDidChangeTextDocument(async event =>
+	{
+		const editor = vscode.window.activeTextEditor;
+
+		if(editor)
+		{
+			const doc = editor.document;
+			const selection = editor.selection;
+
+			let line = doc.lineAt(selection.active.line);
+			let lineText = line.text;
+			let character = lineText.charAt(selection.active.character);
 			const pattern = /\/lorem\s(p|l|w)\s(\d+)/g;
-			let match;
 
-			while((match = pattern.exec(text)) !== null)
+			if(character === ' ')
 			{
-				const type = match[1];
-				const number = parseInt(match[2]);
-
-
-				const start = doc.positionAt(match.index);
-				const end = doc.positionAt(match.index + match[0].length);
-				console.log("end: " + match.index + match[0].length);
-				const range = new vscode.Range(start, end);
-
-				GenLorem(type, number).then(loremText =>
-					{
-						editor.edit(editBuilder =>
-							{
-								editBuilder.replace(range, loremText);
-							});
-					}).catch(error => 
-						{
-							vscode.window.showErrorMessage('Error: '+ error.message +'.');
-						});
-			}
-		});
-
-		context.subscriptions.push(loremipsum);
-
-		*/
-
-		const provideInlineCompletionItems = (document: vscode.TextDocument, position: vscode.Position, context: vscode.InlineCompletionContext) =>
-		{
-			let prefix = document.lineAt(position).text.substr(0, position.character);
-
-			if(prefix.endsWith('/') && !prefix.endsWith("</"))
-			{
-				const slashCompletionItemText = new vscode.InlineCompletionItem("lorem");
-				const slashCompletionItemImage = new vscode.InlineCompletionItem("image");
-
-				return [slashCompletionItemText, slashCompletionItemImage];
-			}
-
-			if(prefix.endsWith("/lorem p ") || prefix.endsWith("/lorem l ") || prefix.endsWith("/lorem w "))
-			{
-				const noPlaceholder = new vscode.InlineCompletionItem("" + Math.round(Math.random() * 21));
-
-				return [noPlaceholder];
-			}
-
-			return [];
-		}
-
-		context.subscriptions.push(vscode.languages.registerInlineCompletionItemProvider(
-			["html", 'php'], 
-			{provideInlineCompletionItems}
-		));
-
-		const provideCompletionItems = (document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) =>
-		{
-			let prefix = document.lineAt(position).text.substr(0, position.character);		
-
-			if(prefix.endsWith("/lorem "))
-			{
-				const completionItems = 
-				[
-					new vscode.CompletionItem("p", vscode.CompletionItemKind.Text),
-					new vscode.CompletionItem("l", vscode.CompletionItemKind.Text),
-					new vscode.CompletionItem("w", vscode.CompletionItemKind.Text)
-				];
-
-				return completionItems;
-			}
-
-			return [];
-		}
-
-		context.subscriptions.push(vscode.languages.registerCompletionItemProvider(
-			["html", 'php'], 
-			{provideCompletionItems},
-			' '
-		));
-
-		var match;
-		var decoration = vscode.window.createTextEditorDecorationType(
-			{
-
-			}
-		);
-
-		vscode.workspace.onDidChangeTextDocument(async event =>
-			{
-				const editor = vscode.window.activeTextEditor;
-
-				if(editor)
+				while ((match = pattern.exec(lineText)) !== null) 
 				{
-					const doc = editor.document;
-					const selection = editor.selection;
+					let start = new vscode.Position(line.lineNumber, line.range.start.character + match.index);
+					let end = new vscode.Position(line.lineNumber, line.range.start.character + match.index + match[0].length);
 
-					let line = doc.lineAt(selection.active.line);
-					let lineText = line.text;
-					let character = lineText.charAt(selection.active.character);
-					const pattern = /\/lorem\s(p|l|w)\s(\d+)/g;
-
-					if(character === ' ')
+					decoration = vscode.window.createTextEditorDecorationType(
 					{
-						while ((match = pattern.exec(lineText)) !== null) 
-						{
-							let start = new vscode.Position(line.lineNumber, line.range.start.character + match.index);
-							let end = new vscode.Position(line.lineNumber, line.range.start.character + match.index + match[0].length);
-
-							decoration = vscode.window.createTextEditorDecorationType(
-							{
-								backgroundColor: 'rgba(100, 100, 100, 0.3)',
-								borderRadius: '4px',
-								borderSpacing: '2px',
+						backgroundColor: 'rgba(100, 100, 100, 0.3)',
+						borderRadius: '4px',
+						borderSpacing: '2px',
 						
-								after:
-								{
-									contentText: " press Enter to replace with  Lorem Ipsum",
-									fontStyle: 'ilatic',
-									color: 'rgb(160, 160, 160)'
-								}
-							});
-
-							console.log("PATTERN DETECTION\ndecoration: ", decoration, "\nstart: ", start, "\nend: ", end);
-				
-							editor.setDecorations(decoration, [new vscode.Range(start, end)]);
-						}
-					}
-
-					if((event.contentChanges[0].text === '\n' || event.contentChanges[0].text === '\r\n') && (match = pattern.exec(lineText)) !== null)
-					{
-						while (match !== null) 
+						after:
 						{
-							let start = new vscode.Position(line.lineNumber, line.range.start.character + match.index);
-							let end = new vscode.Position(line.lineNumber, line.range.start.character + match.index + match[0].length);
-				
-							let loremText = await GenLorem(match[1], parseInt(match[2]));
-				
-							editor.edit(editBuilder => 
-							{
-								editBuilder.replace(new vscode.Range(start, end), loremText);
-							});
-
-							match = pattern.exec(lineText);
+							contentText: " press Enter to replace with  Lorem Ipsum",
+							fontStyle: 'ilatic',
+							color: 'rgb(160, 160, 160)'
 						}
-						decoration.dispose();
-					}
+					});
+				
+					editor.setDecorations(decoration, [new vscode.Range(start, end)]);
 				}
-			});
+			}
+
+			if((event.contentChanges[0].text === '\n' || event.contentChanges[0].text === '\r\n') && (match = pattern.exec(lineText)) !== null)
+			{
+				while (match !== null) 
+				{
+					let start = new vscode.Position(line.lineNumber, line.range.start.character + match.index);
+					let end = new vscode.Position(line.lineNumber, line.range.start.character + match.index + match[0].length);
+				
+					let loremText = await GenLorem(match[1], parseInt(match[2]));
+				
+					editor.edit(editBuilder => 
+					{
+						editBuilder.replace(new vscode.Range(start, end), loremText);
+					});
+
+					match = pattern.exec(lineText);
+				}
+				decoration.dispose();
+			}
+		}
+	});
 }
 
-interface LoremApiResponse {
-    feed: {
+interface LoremApiResponse
+{
+    feed:
+	{
         lipsum: string;
     };
 }
 
-async function GenLorem(type: string, number: number): Promise<string> {
+async function GenLorem(type: string, number: number): Promise<string>
+{
     let url = `https://lipsum.com/feed/json?amount=${number}&what=`;
 
-    switch (type) {
+    switch (type)
+	{
         case 'p':
             url += 'paragraphs';
             break;
